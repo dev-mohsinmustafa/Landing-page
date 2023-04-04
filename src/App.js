816424e0c0915a3903d023d523670f46e9fcdc61
mohsin-mustafa <mohsinmustafaansari@gmail.com>
2023-04-04 20:33:12 +0500

    every thing ok

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-// import { Navbar } from 'flowbite-react';
 import './App.css';
-// import Nav from './components/header/header';
-
-// import Header from './components/header/header';
-// import "./components/header/header.css";
 import Landingpage from './components/landingPage/landingpage';
-// import NavMenu from './components/navbar/navMenu';
-
 
 
 function App() {
   return (
     <div className="App">
       
-      {/* <NavMenu /> */}
-      {/* <Header /> */}
-      {/* <Nav/> */}
+      
       <Landingpage />
     </div>
   );
--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -12,20 +12,19 @@ const Nav = () => {
 
   return (
 
-    <nav className="bg-[#1F2022] border-gray-200 px-2 sm:px-4 py-8 rounded dark:bg-gray-900">
+    <nav className="bg-[#1F2022] px-2 sm:px-4 py-8 ">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <a href="#" className="flex items-center">
           <span className="self-center font-bold font-['Josefin Sans'] 
           text-[24px] leading-[24px] whitespace-nowrap dark:text-white text-[#EEEEEE] 
-          mx-12 hover:text-[#FCE611] ">Jessie.</span>
+          ml-[80px] hover:text-[#FCE611] ">Jessie.</span>
 
         </a>
         <div className="flex md:order-2">
-          <button type="button" class="focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-[3px]
+          <button type="button" className="focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-[3px]
           font-bold font-[Josefin Sans] 
           text-[18px] leading-[18px]
-          
-          text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-[#EEEEEE] hover:bg-yellow-300 text-[background: #000000] ">Schedule a call</button>
+           p-4  text-center mr-3 md:mr-[80px] bg-[#EEEEEE] hover:bg-yellow-300  hidden lg:block">Schedule a call</button>
 
 
           <button onClick={() => setOpen((prev) => !prev)} data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
@@ -42,7 +41,7 @@ const Nav = () => {
 
 
 
-          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-[#1F2022]  dark:border-gray-700">
+          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-9 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-[#1F2022]  dark:border-gray-700">
             <li>
               <a href="#" class="block py-2 pl-3 pr-4   rounded md:bg-transparent md:p-0 dark:text-white 
               hover:text-[#FCE611] text-[#00ADB5] font-bold font-['Josefin Sans'] 
